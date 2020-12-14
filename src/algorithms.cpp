1c0c0215fae89cef9286ed5f00da8fc2c2aaa850
himalaya-dt <hyperzy.yang@gmail.com>
2020-12-14 16:47:37 -0500

    Cleanup some redundant codes.

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -164,21 +164,5 @@ void testAll() {
     displayer->startRender();
     displayer->startInteractor();
 
-//    auto tsdf = make_shared<TSDF>();
-//    tsdf->setIntrinsic(p_depth_img->getIntrinsic());
-//    tsdf->setDelta(0.002);
-//    tsdf->setEta(.002);
-//    tsdf->setPaddingSize(3);
-//    dtype resolution = .002;
-//    cv::Mat mask = p_depth_img->getImage() != INF;
-//    tsdf->Init(p_depth_img->getImage(), mask, resolution);
-//
-//    tsdf->estimateTwist(p_depth_img->getImage(), p_depth_img->getImage(), mask, mask, resolution, 50, .4);
-//
-//    auto displayer = make_shared<Display>();
-//    displayer->Init();
-//    displayer->addAxes();
-//    displayer->addIsoSurface(tsdf->phi, tsdf->getMinCoord(), tsdf->getDepth(), tsdf->getHeight(), tsdf->getWidth(), resolution);
-//    displayer->startRender();
 }
 
